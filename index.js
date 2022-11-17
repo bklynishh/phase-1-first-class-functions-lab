2c91768e4882e190d1ea7fbd7cc679b6aa6025a0
Hannah Maratea <maratea101@gmail.com>
2022-11-17 17:48:52 -0500

    commit tree 696d139bd14271d206a2b202d325bd0151035e2d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+// Declare returnFirstTwoDrivers
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (parameter) {
+    console.log(parameter.slice(0, 2));
+    return parameter.slice(0, 2);
+}
+
+// Declare returnLastTwoDrivers
+const returnLastTwoDrivers = function(parameter) {
+    console.log(parameter.slice(2, 4));
+    return parameter.slice(2, 4);
+}
+
+// selectingDrivers Array
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareMultiplier) {
+  return function(value) {
+    return fareMultiplier * value;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function fetchSpecifiedDrivers(drivers, func) {
+  return func(drivers);
+}
+
+function selectDifferentDrivers(driversArray, fn) {
+    return fn(driversArray);
+}
